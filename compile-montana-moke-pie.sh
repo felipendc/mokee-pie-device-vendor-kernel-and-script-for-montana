1f1e5054e25d5c48e018d2e153aeeacfd345db73
felipendc <felipendc10phoenix@gmail.com>
2020-06-22 12:24:04 -0300

    Update compile-montana-moke-pie.sh

--- compile-montana-moke-pie.sh
+++ compile-montana-moke-pie.sh
@@ -7,5 +7,16 @@
 #device: https://github.com/PixelExperience-Devices/device_motorola_montana
 #vendor: https://github.com/PixelExperience-Devices/vendor_motorola_montana
 
-rm -r out && . build/envsetup.sh && lunch mk_montana-userdebug && mka bacon
+#!/bin/bash
+
+# Git lfs 
+git lfs install 
+
+# Make clean
+sudo rm -r out
+
+# Set up environment and Compile
+. build/envsetup.sh && lunch mk_montana-userdebug && mka bacon -j4
+
+
 
