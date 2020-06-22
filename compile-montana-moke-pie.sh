b2679a52f0f2298879a6d2bb0ad7bc9a66083d5f
felipendc <felipendc10@gmail.com>
2020-06-22 02:19:21 -0300

    Vicyos Rolling Update: (seg jun 22 02:19:21 -03 2020)

--- compile-montana-moke-pie.sh
+++ compile-montana-moke-pie.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #kernel: branch pie https://github.com/AsdMonio/android_kernel_motorola_msm8937 
-#device: 
-#vendor: 
+#device: https://github.com/PixelExperience-Devices/device_motorola_montana
+#vendor: https://github.com/PixelExperience-Devices/vendor_motorola_montana
 
 rm -r out && . build/envsetup.sh && lunch mk_montana-userdebug && mka bacon
 
