fede932ce55aa4766fca6ed42240bfdf3e7e946d
felipendc <felipendc10phoenix@gmail.com>
2020-06-23 01:44:11 -0300

    Update compile-montana-moke-pie.sh

--- compile-montana-moke-pie.sh
+++ compile-montana-moke-pie.sh
@@ -6,6 +6,11 @@
 #device: https://github.com/PixelExperience-Devices/device_motorola_montana
 #vendor: https://github.com/PixelExperience-Devices/vendor_motorola_montana
 
+# TO COMPILE ANDROID 9, "openjdk-8-jdk" IS REQUIRED. RUN THE FOLLOWING COMMNANDS TO INSTALL IT:
+
+sudo apt-get purge openjdk-* icedtea-* icedtea6-* && sudo apt-get update && sudo apt-get install openjdk-8-jdk && java -version
+
+
 # Git lfs 
 git lfs install 
 
