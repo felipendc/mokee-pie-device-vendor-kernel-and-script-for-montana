241f43e53d8d5935e4d8b13a6c70330a23f4d086
felipendc <felipendc10phoenix@gmail.com>
2020-06-22 23:37:42 -0300

    Update compile-montana-moke-pie.sh

--- compile-montana-moke-pie.sh
+++ compile-montana-moke-pie.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-
 # Some of the files from the link below has been modified by Vicyos (Felipe Ndc) to fix some build errors.
 
 #kernel: branch pie https://github.com/AsdMonio/android_kernel_motorola_msm8937 
 #device: https://github.com/PixelExperience-Devices/device_motorola_montana
 #vendor: https://github.com/PixelExperience-Devices/vendor_motorola_montana
 
-#!/bin/bash
-
 # Git lfs 
 git lfs install 
 
-# Make clean
-sudo rm -r out
+# Make clean and set up environment and Compile
+rm -r out && . build/envsetup.sh && lunch mk_montana-userdebug && mka bacon -j4
 
-# Set up environment and Compile
-. build/envsetup.sh && lunch mk_montana-userdebug && mka bacon -j4
 
 
 
