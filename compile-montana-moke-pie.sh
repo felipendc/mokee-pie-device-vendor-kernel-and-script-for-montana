84318c00382203a54605ba42bb8748ea97119a1b
felipendc <felipendc10phoenix@gmail.com>
2020-06-23 09:42:12 -0300

    Update compile-montana-moke-pie.sh

--- compile-montana-moke-pie.sh
+++ compile-montana-moke-pie.sh
@@ -6,11 +6,18 @@
 #device: https://github.com/PixelExperience-Devices/device_motorola_montana
 #vendor: https://github.com/PixelExperience-Devices/vendor_motorola_montana
 
-# TO COMPILE ANDROID 9, "openjdk-8-jdk" IS REQUIRED. RUN THE FOLLOWING COMMNANDS TO INSTALL IT:
 
+NOTES: 
+
+# TO COMPILE ANDROID 9, "openjdk-8-jdk" IS REQUIRED. RUN THE FOLLOWING COMMNANDS TO INSTALL IT:
 sudo apt-get purge openjdk-* icedtea-* icedtea6-* && sudo apt-get update && sudo apt-get install openjdk-8-jdk && java -version
 
+#REMOVE MOTO ACTIONS TEMPORARY:
+rm -r /packages/resources/MotoActions*
+rm -r /device/motorola/montana/MotoActions*
 
+
+# Things to test out:
 # Try to compile with "LLVM 10.0.0" soon!
 # https://releases.llvm.org/download.html#10.0.0
 
